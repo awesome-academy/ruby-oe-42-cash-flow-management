9422253a54652b8e432cc3c99cd95d7ededa7d77
vanvtt-0952 <49464465+vanvtt-0952@users.noreply.github.com>
2021-04-14 08:01:36 +0700

    Revert "test model, controller Budget"

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,6 @@ FactoryBot.define do
   factory :user do
     name { Faker::Alphanumeric.alphanumeric(number: 20) }
     email { Faker::Internet.email }
-    password { "123123" }
-    password_confirmation { "123123" }
+    password = "123123"
   end
 end
